65a9d272fe3d75c3c8197390a960092d01efed95
mishaelajay <mishaelajay@gmail.com>
2025-02-09 23:22:38 +0530

    Improve autobid service

--- app/services/auto_bid_processor.rb
+++ app/services/auto_bid_processor.rb
@@ -6,26 +6,18 @@ class AutoBidProcessor
   def process
     return unless should_process?
 
-    # Get the current highest bid in a single query
-    current_highest_bid = @auction.bids.select(:user_id, :amount).order(amount: :desc).first
-    current_price = current_highest_bid&.amount || @auction.starting_price
-    
-    # Find the next highest auto bid in a single optimized query
-    next_auto_bid = @auction.auto_bids
-      .where('maximum_amount > ? AND user_id != ?', current_price, current_highest_bid&.user_id || 0)
-      .select(:id, :user_id, :maximum_amount)
-      .order(maximum_amount: :desc, created_at: :asc)
-      .lock("FOR UPDATE SKIP LOCKED")
-      .first
+    # Use a single query to get both the current highest bid and the next auto bid
+    current_highest_bid, next_auto_bid = fetch_bids
 
     return unless next_auto_bid
 
     # Calculate and place the bid
-    next_minimum_bid = calculate_next_minimum_bid(current_price)
-    bid_amount = [next_minimum_bid, next_auto_bid.maximum_amount].min
+    current_amount = current_highest_bid ? current_highest_bid['amount'] : @auction.starting_price
+    next_minimum_bid = calculate_next_minimum_bid(current_amount)
+    bid_amount = [next_minimum_bid, next_auto_bid['maximum_amount']].min
     
     Bid.create!(
-      user_id: next_auto_bid.user_id,
+      user_id: next_auto_bid['auto_bid_user_id'],
       auction_id: @auction.id,
       amount: bid_amount
     )
@@ -36,6 +28,25 @@ class AutoBidProcessor
   def should_process?
     @auction.active? && 
     @auction.auto_bids.where('maximum_amount > ?', @auction.current_price).exists?
+  end
+
+  def fetch_bids
+    current_price = @auction.bids.maximum(:amount) || @auction.starting_price
+
+    # Use a single query to fetch both the current highest bid and the next auto bid
+    bids = @auction.bids
+      .select('bids.user_id, bids.amount, auto_bids.id AS auto_bid_id, auto_bids.user_id AS auto_bid_user_id, auto_bids.maximum_amount')
+      .joins('LEFT JOIN auto_bids ON auto_bids.auction_id = bids.auction_id')
+      .where('auto_bids.maximum_amount > ? AND auto_bids.user_id != bids.user_id', current_price)
+      .order('bids.amount DESC, auto_bids.maximum_amount DESC, auto_bids.created_at ASC')
+      .lock('FOR UPDATE SKIP LOCKED')
+      .limit(1)
+      .first
+
+    current_highest_bid = bids&.slice(:user_id, :amount)
+    next_auto_bid = bids&.slice(:auto_bid_id, :auto_bid_user_id, :maximum_amount)
+
+    [current_highest_bid, next_auto_bid]
   end
 
   def calculate_next_minimum_bid(current_price)
